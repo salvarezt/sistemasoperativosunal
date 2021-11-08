f2d4a4bfb4c4adc54981e0029d2674ea7075dbb3
salvarezt <92274757+salvarezt@users.noreply.github.com>
2021-11-08 01:41:51 -0500

    Add files via upload

--- parcial1/punto4/cliente.py
+++ parcial1/punto4/cliente.py
@@ -1,1 +1,84 @@
+import os
+import socket
+import asyncio
+
+host, port = 'localhost', 10000
+
+class Cliente:
+    def __init__(self, sock = None):
+        if sock == None:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock = sock
+        
+    def conectar(self, host, port):
+        try:
+            self.sock.connect( (host, port) )
+            print('¡Conexion lograda!')
+        except os.error:
+            print('Hubo un error al conectar. ' + os.error)
+    
+    def guardarArchivo(self, narchivo = ''):
+        # narchivo es el nombre del archivo a guardar en el server
+        if narchivo == '':
+            print('Debes ingresar un archivo para enviar')
+            return 0
+        try:
+            f = open(f'carpetaCliente/{narchivo}', 'r')
+            p = f.read()
+            comando = 'SAVE ' + narchivo
+            self.sock.send(comando.encode('utf-8')) # Guardar archivo
+            self.sock.send(p.encode('utf-8'))
+
+            mensajeRecibido = self.sock.recv(50) # Queremos saber que no hubo problemas para recibir el mensaje
+            print(mensajeRecibido.decode('utf-8'))
+        except os.error:
+            print('Hubo un error al enviar el archivo ' + os.error)
+            return 1
+    
+    def recibirArchivo(self, narchivo = ''):
+        # narchivo es el nombre de los archivos a recibir
+        if narchivo == '':
+            print('No se ha pedido ningun archivo')
+            return 0
+        elif os.path.exists(f'carpetaCliente/{narchivo}'):
+            print('El archivo que intentas recibir ya existe en tu carpeta\nEste archivo sera re-escrito')
+        comando = 'GET ' + narchivo
+        try:
+            self.sock.send(comando.encode('utf-8'))
+            f = open(f'carpetaCliente/{narchivo}', 'w')
+            mensaje = self.sock.recv(50).decode('utf-8')
+            print(mensaje)
+            if mensaje != 'Recibiendo Archivo':
+                return 1
+            temp = self.sock.recv(500).decode('utf-8')
+            f.write(temp + '\n')
+            print('Archivos recibidos con exito')
+            f.close()
+            return 0
+        except os.error:
+            print('Hubo un error al obtener los archivos: ' + os.error)
+            return 1
+
+    def eliminarArchivo(self, narchivo = ''):
+        if narchivo == '':
+            print('Debes ingresar el nombre del archivo a eliminar')
+            return 0
+        try:
+            comando = 'DEL ' + narchivo
+            self.sock.send(comando.encode('utf-8'))
+            mensajeRecibido = self.sock.recv(50) # Queremos saber que no hubo problemas para eliminar
+            print(mensajeRecibido.decode('utf-8'))
+            return 0
+        except os.error:
+            print('Hubo un error eliminando el archivo: ' + os.error)
+            return 1
+
+    def desconectar(self):
+        self.sock.close()
+
+cliente = Cliente()
+cliente.conectar(host, port)
+cliente.recibirArchivo('texto.txt')
+#cliente.eliminarArchivo('texto.txt')
+cliente.desconectar()
 
---
+++ parcial1/punto4/servidor.py
@@ -0,0 +1,80 @@
+import socket
+import os
+import asyncio
+
+host, port = 'localhost', 10000
+
+class Server:
+    def __init__(self, sock = None):
+        if sock == None:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock = sock
+        self.clientes = []
+        self.archivos = []
+    
+    def conexion(self, host, port):
+        try:
+            self.sock.bind( (host, port) )
+            print('Server corriendo\n')
+        except os.error:
+            print('Hubo un error al conectar. ' + os.error)
+    
+    def recibirConexion(self):
+        conn, addr = self.sock.accept()
+        self.clientes.append(conn)
+        print('¡Se ha conectado un cliente nuevo!')
+        print(f'{addr=}\n')
+    
+    def manejarPedido(self, cliente):
+        # En realidad solamente hay las siguientes posibilidades:
+        # comando = 'SAVE ' + narchivo
+        # comando = 'GET ' + narchivos
+        # comando = 'DEL ' + narchivo
+        # Manejaremos cada una de estas posibilidades por separado
+
+        pedido = cliente.recv(100).decode('utf-8').split(' ')
+        if pedido[0] == 'SAVE':
+            pass
+        if pedido[0] == 'GET':
+            self.pedidoRecibir(cliente, pedido)
+        if pedido[0] == 'DEL':
+            self.pedidoBorrar(cliente, pedido)
+            # Borrarmos el archivo del servidor
+        print('Pedido correctamente manejado')
+        return 0
+
+    #def pedidoGuardar(self, cliente, pedido):
+        
+    
+    def pedidoRecibir(self, cliente, pedido):
+        try:
+            print(f'enviaremos el archivo {pedido[1]}')
+            f = open(f'carpetaServer/{pedido[1]}', 'r')
+            mensaje = 'Recibiendo Archivo'.encode('utf-8')
+            cliente.send(mensaje)
+            archivo = f.read()
+            cliente.send(archivo.encode('utf-8'))
+            f.close()
+            print('¡Archivo enviado correctamente!')
+        except FileNotFoundError:
+            cliente.send('El archivo que buscas no existe en el servidor :c'.encode('utf-8'))
+            return 1
+    
+    def pedidoBorrar(self, cliente, pedido):
+        try:
+            os.remove(f'carpetaServer/{pedido[1]}')
+            Mensaje = '¡Archivo Eliminado Correctamente!'
+            cliente.send(Mensaje.encode('utf-8'))
+        except os.error:
+            Mensaje = 'Ha habido un error al eliminar: ' + os.error
+            cliente.send(Mensaje.encode('utf-8'))
+            return 1
+
+server = Server()
+server.conexion(host, port)
+server.sock.listen(5)
+
+while True:
+    server.recibirConexion()
+    for i in server.clientes:
+        server.manejarPedido(i)
