ac83afd48224bc917ee959a6e8d5186f27cc22f5
Samuel Alvarez <salvarezt@unal.edu.co>
2022-02-03 08:42:32 -0500

    webSockets

---
+++ webSockets/frontend/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RoomComponent } from './room/room.component';
+import { HomeComponent } from './home/home.component';
+import { CookieService } from 'ngx-cookie-service';
+import { DrawComponent } from './draw/draw.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RoomComponent,
+    HomeComponent,
+    DrawComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [CookieService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
